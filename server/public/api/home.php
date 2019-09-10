5eb21ff2ff8d74f2feea639057649d924b0a661f
Mona-S <forward2cm@gmail.com>
2019-09-10 20:00:43 +0000

    checked if the id is a number or not

--- server/public/api/home.php
+++ server/public/api/home.php
@@ -7,3 +7,14 @@ startup();
 set_error_handler('error_handler');
 
 require_once('./db_connection.php');
+
+if(empty($_GET['goal_id'])) {
+    $whereClause = '';
+} 
+else if (!is_numeric($_GET['goal_id'])) {
+    throw new Exception("id needs to be a number");
+} 
+else {
+    $whereClause = "WHERE goal_details.goal_id = {$_GET['goal_id']}";
+}
+  
