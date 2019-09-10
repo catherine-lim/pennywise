3ddcd64b9b33b512756a426d5326efa03b570161
Alex Vong <avong003@gmail.com>
2019-09-10 14:58:34 -0700

    Add a componentDidMount method to GoalDetails that retrieves the details of a specific goal by id from the server with a GET request. e.g. /api/goals.php?id=1

--- client/components/goal-details.jsx
+++ client/components/goal-details.jsx
@@ -8,10 +8,14 @@ export default class GoalDetails extends React.Component {
     };
   }
 
-  render() {
-
-    return <h1>Hello World</h1>;
-
+  componentDidMount(props) {
+    const currentParam = this.props.params.id;
+    fetch(`/api/goals.php?id=` + currentParam)
+      .then(res => res.json())
+      .then(response => this.setState({ product: response }));
   }
 
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
