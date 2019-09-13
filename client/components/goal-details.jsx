11bac7f3f57acb27e869fb06c8fdcc010d001e73
Alex Vong <avong003@gmail.com>
2019-09-13 00:27:28 +0000

    made button and input

--- client/components/goal-details.jsx
+++ client/components/goal-details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import GoalCard from 'goal-card';
-import { dailyGoal } from './helper.js';
+// import GoalCard from 'goal-card';
+import { dailyGoal, differenceInDays, weeklyGoal, inDollars } from './helper.js';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export default class GoalDetails extends React.Component {
   constructor(props) {
@@ -8,17 +9,11 @@ export default class GoalDetails extends React.Component {
     this.state = {
       goal: []
     };
-    this.getGoal = this.getGoal.bind(this);
-
   }
 
   componentDidMount(props) {
     this.getGoal();
   }
-
-  // getDayRemaining() {
-  //   var days = differenceInDays(this.state.goal);
-  // }
 
   getGoal() {
     // const currentParam = this.props.params.id;
@@ -28,19 +23,111 @@ export default class GoalDetails extends React.Component {
       .then(response => this.setState({ goal: response }));
   }
 
-  render(goalData) {
+  getProgress() {
+    const percent = this.state.goal.current_savings / this.state.goal.savings_target;
+    const now = Math.round(percent * 100);
+    return (
+      <div className="progressBar">
+        <ProgressBar
+          className="progressBar"
+          variant="success"
+          now={now}
+          label={`${now}%`}
+        />
+      </div>
+    );
+  }
+
+  newDailyGoal() {
+    return (
+      <div className="dailyGoal">
+        {dailyGoal(this.state.goal)}
+        <div className="day"> /day </div>
+      </div>
+    );
+  }
+
+  newWeeklyGoal() {
+    return (
+      <div className="weeklyGoal">
+        {weeklyGoal(this.state.goal)}
+        <div className="week">/days</div>
+      </div>
+    );
+  }
+
+  towardsSavings() {
+    return (
+      <div className="-toward-6000">
+        {inDollars(this.state.goal.current_savings)} toward
+        {inDollars(this.state.goal.savings_target)}
+      </div>
+    );
+  }
+
+  addOrRemoveSavings() {
+    return (
+      <div className="add-or-remove-funds">
+        <input type="text mt-5" placeholder="add or remove funds"></input>
+        <div className="Line"></div>
+      </div>
+    );
+  }
+
+  addOrRemoveButtons() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="add-button"></div>
+          <div className="subtract-button"></div>
+        </div>
+      </div>
+    );
+  }
+
+  getCardTitle() {
+    return (
+      <div className="goal-card-title teal">
+        <span className="goal-card"> {this.state.goal.goal_name}</span>
+      </div>
+    );
+  }
+
+  getDaysRemaining() {
+    return (
+      <div className="daysRemaining">{differenceInDays(this.state.goal)} days</div>
+    );
+  }
+
+  makeTransactionHistory() {
+    return (
+      <div>
+        <div className="transaction-history">Transaction History</div>
+        <div className="Line2"></div>
+      </div>
+    );
+  }
+
+  getEverything() {
     return (
       <React.Fragment>
-        return <GoalCard
-          key={goalData.goal_id}
-          id={goalData.goal_id}
-          name={goalData.goal_name}
-          completionDate={goalData.goal_completion_date}
-          savingsTarget={goalData.savings_target}
-          currentSavings={goalData.current_savings}
-          dailyGoal={dailyGoal(goalData)}
-          isCompleted={goalData.isCompleted}/>
+        {this.getDaysRemaining()}
+        {this.getCardTitle()}
+        {this.getProgress()}
+        {this.towardsSavings()}
+        {this.newDailyGoal()}
+        {this.newWeeklyGoal()}
+        {this.addOrRemoveSavings()}
+        {this.addOrRemoveButtons()}
+        {this.makeTransactionHistory()}
+      </React.Fragment>
+    );
+  }
 
+  render() {
+    return (
+      <React.Fragment>
+        {this.getEverything()}
       </React.Fragment>
     );
   }
