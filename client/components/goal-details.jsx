ce59361d10c53b7ef440ad7d74477b71aac3da45
Alex Vong <avong003@gmail.com>
2019-09-19 23:46:14 +0000

    penny wise logo

--- client/components/goal-details.jsx
+++ client/components/goal-details.jsx
@@ -64,18 +64,26 @@ export default class GoalDetails extends React.Component {
 
   newDailyAndWeeklyGoal() {
     return (
-      <div>
+      <div className="card-container">
+        <div className="goals-row">
 
-        <div>
-          <div className="dailyGoal">{inDollars(dailyGoal(this.state))}</div>
-          <div className="dailyDays"> /day </div>
+          <div className="goals-total-container">
+            <div className="daily-totals-text">
+              {inDollars(dailyGoal(this.state))}
+            </div>
+            <div className="per-day-text">
+                /day
+            </div>
+          </div>
+          <div className="goals-total-container">
+            <div className="weekly-totals-text">
+              {inDollars(weeklyGoal(this.state))}
+            </div>
+            <div className="per-week-text">
+              /week
+            </div>
+          </div>
         </div>
-
-        <div>
-          <div className="weeklyGoal">{inDollars(weeklyGoal(this.state))}</div>
-          <div className="weeklyWeeks"> /week </div>
-        </div>
-
       </div>
     );
   }
